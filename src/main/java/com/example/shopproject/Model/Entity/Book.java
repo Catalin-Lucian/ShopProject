cee8825c7de7301ec1590076b424c4806ff9b9b7
Lucian <63058063+Catalin-Lucian@users.noreply.github.com>
2021-11-05 19:47:46 +0200

    test pt andreea

--- src/main/java/com/example/shopproject/Model/Entity/Book.java
+++ src/main/java/com/example/shopproject/Model/Entity/Book.java
@@ -10,7 +10,7 @@ public class Book {
 
 
     public Book() {
-        //test pt cezar 
+        //test pt andreea
     }
 
     public Book(String ISBN, String titlu, String editura, Integer anPublicare, String genLiteral) {
